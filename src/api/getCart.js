4b50f7f8c8f2b9ff12d3cbd38d11589340a46fc3
Luis Ruiz <lerc2990@gmail.com>
2019-02-01 20:44:02 -0500

    Se agrego nuevo servicio para obtener el carro

---
+++ src/api/getCart.js
@@ -0,0 +1,25 @@
+const dynamo = require('../awsUtils/dynamoUtils');
+
+module.exports.main = async (event, context) => {
+
+  let customerId = event.pathParameters.id;
+  
+  let mybody = await dynamo.getCartFromCustomer(customerId);
+  
+  console.log(mybody);
+
+  let response = {
+    statusCode: 200,
+    headers: {
+      "X-Requested-With": '*',
+      "Access-Control-Allow-Headers": 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,x-requested-with',
+      "Access-Control-Allow-Origin": '*',
+      "Access-Control-Allow-Methods": 'POST,GET,OPTIONS',
+      "Access-Control-Allow-Credentials" : true
+    },
+    body: JSON.stringify(mybody)
+  }
+
+  return response;
+
+}
